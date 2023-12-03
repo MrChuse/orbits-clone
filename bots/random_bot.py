1218c0dcb765cbe5044038052cd0e0b100abc200
lukashevichusaa <lukashevichusaa@voice-link.ru>
2023-12-03 13:52:17 +0300

    Changed RandomBot to remove inheritance from helper class, use __init__ instead

--- bots/random_bot.py
+++ bots/random_bot.py
@@ -7,18 +7,17 @@ from . import Bot
 
 # Do not forget to add your bot to __init__ file.
 # Import it there and add to the bots list
-
 class RandomBot():
     def __init__(self, cutoff):
         self.cutoff = cutoff
         self.__name__ = f'RandomBot{cutoff}'
 
     def __call__(self, center, velocity, radius, color):
-        class RandomBotChild(RandomBotThing):
-            cutoff = self.cutoff
-        return RandomBotChild(center, velocity, radius, color)
+        return RandomBotThing(self.cutoff, center, velocity, radius, color)
 
 class RandomBotThing(Bot):
-    cutoff = 0.1
+    def __init__(self, cutoff, center, velocity, radius, color):
+        super().__init__(center, velocity, radius, color)
+        self.cutoff = cutoff
     def get_action(self, state: GameState, time_delta: float) -> bool:
         return random.random() < self.cutoff
